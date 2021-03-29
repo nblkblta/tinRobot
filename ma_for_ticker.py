6897c4ef30c3c3c4d64fcc96915bbdac0817c438
Nick <nblkdjum@gmail.com>
2021-03-30 08:49:54 +1000

    Теперь get_figi_data не имеет ограничений по периоду

--- ma_for_ticker.py
+++ ma_for_ticker.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import datetime, timedelta
 from typing import List, Tuple
@@ -10,37 +9,38 @@ import tinvest as ti
 
 client = ti.SyncClient(os.getenv("TINVEST_TOKEN", ''))
 interval = ti.CandleResolution.day
-period = 300
-to = datetime.now()
-fr = to - timedelta(days=period)
-ma_period = [10,60]
+ma_period = [10, 60, 200]
 eps = 0.03
 
+
 def main() -> None:
+    period = 5000
     ticker = "BABA"
     tester(get_figi_by_ticker(ticker))
-    get_graph_by_ticker(ticker)
+    get_graph_by_ticker(ticker,period)
 
-def get_figure(figis: List[Tuple[str, str]]) -> go.Figure:
+
+def get_figure(figis: List[Tuple[str, str]],period: int) -> go.Figure:
     return go.Figure(
-        data=[get_candlesstick(get_figi_data(figi), figi, name) for figi, name in figis]
+        data=[get_candlesstick(get_figi_data(figi,period), figi, name) for figi, name in figis]
     )
 
 
-def get_figi_by_ticker(ticker:str):
+def get_figi_by_ticker(ticker: str):
     return client.get_market_search_by_ticker(ticker).payload.instruments[0].figi
 
 
-def get_graph_by_ticker(ticker: str):
+def get_graph_by_ticker(ticker: str, period: int):
     get = client.get_market_search_by_ticker(ticker)
     get = [(get.payload.instruments[0].figi, get.payload.instruments[0].name)]
-    fig = get_figure(get)
-    ma = get_figi_data(get[0])
-    for period in ma_period:
-        fig.add_scatter(name="EMoving average " + str(period), y=ma["c"].ewm(span=period,adjust=False).mean(), x=ma["time"])
-    for period in ma_period:
-        fig.add_scatter(name="Moving average " + str(period), y=ma["c"].rolling(window=period).mean(), x=ma["time"])
+    fig = get_figure(get,period)
+    ma = get_figi_data(get[0],period)
+    for per in ma_period:
+        fig.add_scatter(name="EMoving average " + str(per), y=ma["c"].ewm(span=per, adjust=False).mean(), x=ma["time"])
+    for per in ma_period:
+        fig.add_scatter(name="Moving average " + str(per), y=ma["c"].rolling(window=per).mean(), x=ma["time"])
     fig.show()
+
 
 def get_candlesstick(df: pd.DataFrame, figi: str, name: str) -> go.Candlestick:
     return go.Candlestick(
@@ -53,61 +53,70 @@ def get_candlesstick(df: pd.DataFrame, figi: str, name: str) -> go.Candlestick:
     )
 
 
-def get_figi_data(figi: str) -> pd.DataFrame:
+def get_figi_data(figi: str, period: int) -> pd.DataFrame:
+    local_period = period
     payload = client.get_market_candles(
-        figi=figi,
-        from_=fr,
-        to=to,
-        interval=interval,
-    ).payload
+            figi=figi,
+            from_=datetime.now() - timedelta(days=local_period),
+            to=datetime.now() - timedelta(days=local_period) + timedelta(days=365),
+            interval=interval,
+        ).payload
+    while local_period > 365:
+        local_period -= 365
+        new = client.get_market_candles(
+            figi=figi,
+            from_=datetime.now() - timedelta(days=local_period),
+            to=datetime.now() - timedelta(days=local_period) + timedelta(days=365),
+            interval=interval,
+        ).payload
+        for candle in new.candles:
+            payload.candles.append(candle)
     return pd.DataFrame(c.dict() for c in payload.candles)
 
 
-def get_buy_points(figi:str) -> List:
-    df = get_figi_data(figi)
-    data = [df["c"].rolling(window = 10).mean(),df["c"].rolling(window = 60).mean(),df["time"]]
+def get_buy_points(figi: str, period) -> List:
+    df = get_figi_data(figi, period)
+    data = [df["c"].rolling(window=10).mean(), df["c"].rolling(window=60).mean(), df["time"]]
     result = []
     for i in range(len(data[0])):
-        if -eps<((data[0][i]-data[1][i])/data[1][i])<eps:
-            if data[0][i-1]<data[0][i]:
+        if -eps < ((data[0][i] - data[1][i]) / data[1][i]) < eps:
+            if data[0][i - 1] < data[0][i]:
                 result.append(datetime.date(data[2][i]))
-    print(result)
     return result
 
 
-def get_sell_points(figi:str) -> List:
-    df = get_figi_data(figi)
-    data = [df["c"].rolling(window = 10).mean(),df["c"].rolling(window = 60).mean(),df["time"]]
+def get_sell_points(figi: str, period: int) -> List:
+    df = get_figi_data(figi, period)
+    data = [df["c"].rolling(window=10).mean(), df["c"].rolling(window=60).mean(), df["time"]]
     result = []
     for i in range(len(data[0])):
-        if -eps<((data[1][i]-data[0][i])/data[1][i])<eps:
-            if data[0][i-1] > data[0][i]:
+        if -eps < ((data[1][i] - data[0][i]) / data[1][i]) < eps:
+            if data[0][i - 1] > data[0][i]:
                 result.append(datetime.date(data[2][i]))
-    print(result)
     return result
 
-def tester(figi: str)-> int:
-    df = get_figi_data(figi)
-    print (df)
-    sell_points = get_sell_points(figi)
-    buy_points = get_buy_points(figi)
-    result =[]
-    s=0
-    b=0
+
+def tester(figi: str) -> int:
+    period = 5000
+    df = get_figi_data(figi,period)
+    sell_points = get_sell_points(figi,period)
+    buy_points = get_buy_points(figi,period)
+    result = []
+    s = 0
+    b = 0
     result.append(buy_points[b])
     flag = 1
-    while(s < len(sell_points)-1)and(b<len(buy_points)-1):
-            if flag:
-                if (sell_points[s] > result[len(result) - 1]):
-                    flag=0
-                    result.append(sell_points[s])
-                s += 1
-            else:
-                if (buy_points[b] > result[len(result) - 1]):
-                    flag=1
-                    result.append(buy_points[b])
-                b+=1
-    print (result)
+    while (s < len(sell_points) - 1) and (b < len(buy_points) - 1):
+        if flag:
+            if sell_points[s] > result[len(result) - 1]:
+                flag = 0
+                result.append(sell_points[s])
+            s += 1
+        else:
+            if buy_points[b] > result[len(result) - 1]:
+                flag = 1
+                result.append(buy_points[b])
+            b += 1
 
     return 0
 
