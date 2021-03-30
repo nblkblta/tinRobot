573b36527065bdbff8ac8f20851885d3e4e65f6b
Nick <nblkdjum@gmail.com>
2021-03-30 11:14:22 +1000

    Refactoring

--- ma_for_ticker.py
+++ ma_for_ticker.py
@@ -10,19 +10,19 @@ import tinvest as ti
 client = ti.SyncClient(os.getenv("TINVEST_TOKEN", ''))
 interval = ti.CandleResolution.day
 ma_period = [10, 60]
-eps = 0.01
+eps = 0.10
 
 
 def main() -> None:
     period = 2000
     ticker = "BABA"
-    print(tester(get_figi_by_ticker(ticker),period))
-    get_graph_by_ticker(ticker,period)
+    print(tester(get_figi_by_ticker(ticker), period))
+    get_graph_by_ticker(ticker, period)
 
 
-def get_figure(figis: List[Tuple[str, str]],period: int) -> go.Figure:
+def get_figure(figis: List[Tuple[str, str]], period: int) -> go.Figure:
     return go.Figure(
-        data=[get_candlesstick(get_figi_data(figi,period), figi, name) for figi, name in figis]
+        data=[get_candlesstick(get_figi_data(figi, period), figi, name) for figi, name in figis]
     )
 
 
@@ -33,10 +33,10 @@ def get_figi_by_ticker(ticker: str):
 def get_graph_by_ticker(ticker: str, period: int):
     get = client.get_market_search_by_ticker(ticker)
     get = [(get.payload.instruments[0].figi, get.payload.instruments[0].name)]
-    fig = get_figure(get,period)
-    ma = get_figi_data(get[0],period)
+    fig = get_figure(get, period)
+    ma = get_figi_data(get[0], period)
     for per in ma_period:
-        #fig.add_scatter(name="EMoving average " + str(per), y=ma["c"].ewm(span=per, adjust=False).mean(), x=ma["time"])
+        fig.add_scatter(name="EMoving average " + str(per), y=ma["c"].ewm(span=per, adjust=False).mean(), x=ma["time"])
         fig.add_scatter(name="Moving average " + str(per), y=ma["c"].rolling(window=per).mean(), x=ma["time"])
     fig.show()
 
@@ -55,11 +55,11 @@ def get_candlesstick(df: pd.DataFrame, figi: str, name: str) -> go.Candlestick:
 def get_figi_data(figi: str, period: int) -> pd.DataFrame:
     local_period = period
     payload = client.get_market_candles(
-            figi=figi,
-            from_=datetime.now() - timedelta(days=local_period),
-            to=datetime.now() - timedelta(days=local_period) + timedelta(days=365),
-            interval=interval,
-        ).payload
+        figi=figi,
+        from_=datetime.now() - timedelta(days=local_period),
+        to=datetime.now() - timedelta(days=local_period) + timedelta(days=365),
+        interval=interval,
+    ).payload
     while local_period > 365:
         local_period -= 365
         new = client.get_market_candles(
@@ -96,10 +96,11 @@ def get_sell_points(figi: str, period: int) -> List:
 
 
 def tester(figi: str, period: int) -> int:
-    df = get_figi_data(figi,period)
-    sell_points = get_sell_points(figi,period)
-    buy_points = get_buy_points(figi,period)
-    if(len(buy_points) == 0)or(len(sell_points) == 0):
+    sell_price, buy_price = 0, 0
+    df = get_figi_data(figi, period)
+    sell_points = get_sell_points(figi, period)
+    buy_points = get_buy_points(figi, period)
+    if (len(buy_points) == 0) or (len(sell_points) == 0):
         return 0
     sequence = []
     s, b = 0, 0
@@ -116,19 +117,19 @@ def tester(figi: str, period: int) -> int:
                 flag = 1
                 sequence.append(buy_points[b])
             b += 1
-    prices = [[datetime.date(val[6]),(val[0]+val[5])/2] for val in df.values]
+    prices = [[datetime.date(val[6]), (val[0] + val[5]) / 2] for val in df.values]
     result = 1
     i = 0
-    while i<(len(sequence)/2):
+    while i < (len(sequence) / 2):
         buy_date = sequence[i]
-        sell_date = sequence[i+1]
+        sell_date = sequence[i + 1]
         for price in prices:
-            if price[0]==buy_date:
-                buy_price=price[1]
-            if price[0]==sell_date:
-                sell_price=price[1]
-        result = result * (1+(sell_price-buy_price)/buy_price)
-        i+=1
+            if price[0] == buy_date:
+                buy_price = price[1]
+            if price[0] == sell_date:
+                sell_price = price[1]
+        result = result * (1 + (sell_price - buy_price) / buy_price)
+        i += 1
     print('Количество сделок = ', i)
     return result
 
