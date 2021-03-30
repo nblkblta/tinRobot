482a6381de5cb45e4b3c27573e221e298bf0a512
Nick <nblkdjum@gmail.com>
2021-03-30 13:41:01 +1000

    fix bugs, add abstract class Strategy

--- ma_for_ticker.py
+++ ma_for_ticker.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timedelta
 from typing import List, Tuple
-
+from abc import abstractmethod
 import pandas as pd
 import plotly.graph_objects as go
 
@@ -9,15 +9,72 @@ import tinvest as ti
 
 client = ti.SyncClient(os.getenv("TINVEST_TOKEN", ''))
 interval = ti.CandleResolution.day
-ma_period = [10, 60]
-eps = 0.10
+ma_period = [5, 60]
+eps = 0.05
+
+
+class Strategy(object):
+
+    @abstractmethod
+    def get_buy_points(figi: str, period):
+        pass
+
+    def get_sell_points(figi: str, period):
+        pass
+
+
+class ma_strategy(Strategy):
+
+    def get_buy_points(figi: str, period) -> List:
+        df = get_figi_data(figi, period)
+        data = [df["c"].rolling(window=ma_period[0]).mean(), df["c"].rolling(window=ma_period[1]).mean(), df["time"]]
+        result = []
+        for i in range(len(data[0])):
+            if -eps < ((data[0][i] - data[1][i]) / data[1][i]) < eps:
+                if data[0][i - 1] < data[0][i]:
+                    result.append(datetime.date(data[2][i]))
+        return result
+
+    def get_sell_points(figi: str, period: int) -> List:
+        df = get_figi_data(figi, period)
+        data = [df["c"].rolling(window=ma_period[0]).mean(), df["c"].rolling(window=ma_period[1]).mean(), df["time"]]
+        result = []
+        for i in range(len(data[0])):
+            if -eps < ((data[1][i] - data[0][i]) / data[1][i]) < eps:
+                if data[0][i - 1] > data[0][i]:
+                    result.append(datetime.date(data[2][i]))
+        return result
+
+
+class cross_strategy(Strategy):
+
+    def get_buy_points(figi: str, period) -> List:
+        df = get_figi_data(figi, period)
+        data = [df["c"].rolling(window=ma_period[0]).mean(), df["c"].rolling(window=ma_period[1]).mean(), df["time"]]
+        result = []
+        for i in range(len(data[0])):
+            if ((data[0][i] - data[1][i]) > 0 and (data[0][i - 1] - data[1][i - 1]) < 0):
+                result.append(datetime.date(data[2][i]))
+        return result
+
+    def get_sell_points(figi: str, period: int) -> List:
+        df = get_figi_data(figi, period)
+        data = [df["c"].rolling(window=ma_period[0]).mean(), df["c"].rolling(window=ma_period[1]).mean(), df["time"]]
+        result = []
+        for i in range(len(data[0])):
+            if ((data[0][i] - data[1][i]) < 0 and (data[0][i - 1] - data[1][i - 1]) > 0):
+                result.append(datetime.date(data[2][i]))
+        return result
 
 
 def main() -> None:
-    period = 2000
+    period = 500
     ticker = "BABA"
-    print(tester(get_figi_by_ticker(ticker), period))
-    get_graph_by_ticker(ticker, period)
+    figi = get_figi_by_ticker(ticker)
+    print(tester(figi, period, cross_strategy))
+    #get_graph_by_ticker(ticker, period)
+    print (cross_strategy.get_buy_points(figi,period))
+    print(cross_strategy.get_sell_points(figi,period))
 
 
 def get_figure(figis: List[Tuple[str, str]], period: int) -> go.Figure:
@@ -36,7 +93,7 @@ def get_graph_by_ticker(ticker: str, period: int):
     fig = get_figure(get, period)
     ma = get_figi_data(get[0], period)
     for per in ma_period:
-        fig.add_scatter(name="EMoving average " + str(per), y=ma["c"].ewm(span=per, adjust=False).mean(), x=ma["time"])
+        # fig.add_scatter(name="EMoving average " + str(per), y=ma["c"].ewm(span=per, adjust=False).mean(), x=ma["time"])
         fig.add_scatter(name="Moving average " + str(per), y=ma["c"].rolling(window=per).mean(), x=ma["time"])
     fig.show()
 
@@ -73,40 +130,17 @@ def get_figi_data(figi: str, period: int) -> pd.DataFrame:
     return pd.DataFrame(c.dict() for c in payload.candles)
 
 
-def get_buy_points(figi: str, period) -> List:
-    df = get_figi_data(figi, period)
-    data = [df["c"].rolling(window=10).mean(), df["c"].rolling(window=60).mean(), df["time"]]
-    result = []
-    for i in range(len(data[0])):
-        if -eps < ((data[0][i] - data[1][i]) / data[1][i]) < eps:
-            if data[0][i - 1] < data[0][i]:
-                result.append(datetime.date(data[2][i]))
-    return result
-
-
-def get_sell_points(figi: str, period: int) -> List:
-    df = get_figi_data(figi, period)
-    data = [df["c"].rolling(window=10).mean(), df["c"].rolling(window=60).mean(), df["time"]]
-    result = []
-    for i in range(len(data[0])):
-        if -eps < ((data[1][i] - data[0][i]) / data[1][i]) < eps:
-            if data[0][i - 1] > data[0][i]:
-                result.append(datetime.date(data[2][i]))
-    return result
-
-
-def tester(figi: str, period: int) -> int:
+def tester(figi: str, period: int, strategy: Strategy) -> int:
     sell_price, buy_price = 0, 0
     df = get_figi_data(figi, period)
-    sell_points = get_sell_points(figi, period)
-    buy_points = get_buy_points(figi, period)
+    sell_points, buy_points = strategy.get_sell_points(figi, period), strategy.get_buy_points(figi, period)
     if (len(buy_points) == 0) or (len(sell_points) == 0):
         return 0
     sequence = []
     s, b = 0, 0
     sequence.append(buy_points[b])
     flag = 1
-    while (s < len(sell_points) - 1) and (b < len(buy_points) - 1):
+    while (s <= len(sell_points) - 1) and (b <= len(buy_points) - 1):
         if flag:
             if sell_points[s] > sequence[len(sequence) - 1]:
                 flag = 0
@@ -120,6 +154,7 @@ def tester(figi: str, period: int) -> int:
     prices = [[datetime.date(val[6]), (val[0] + val[5]) / 2] for val in df.values]
     result = 1
     i = 0
+    print(sequence)
     while i < (len(sequence) / 2):
         buy_date = sequence[i]
         sell_date = sequence[i + 1]
@@ -128,6 +163,7 @@ def tester(figi: str, period: int) -> int:
                 buy_price = price[1]
             if price[0] == sell_date:
                 sell_price = price[1]
+        print(buy_price, sell_price)
         result = result * (1 + (sell_price - buy_price) / buy_price)
         i += 1
     print('Количество сделок = ', i)
